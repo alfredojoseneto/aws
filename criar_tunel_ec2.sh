940d3ca92ffd5848a60e93ddb22ca55e98dec8ee
alfredojoseneto <alfredojoseneto@gmail.com>
2025-03-07 01:03:33 +0000

    ADD: added script to create tunnel to ec2 instance

---
+++ criar_tunel_ec2.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+PROFILE=${1}
+INSTANCE_NAME=${2}
+
+if [[ -z ${PROFILE} || -z ${INSTANCE_NAME} ]]; then
+    echo "Primeiro argumento 'PROFILE' ou Segundo argumento 'INSTANCE_NAME' não foi informado"
+    exit 1
+fi
+
+INSTANCE_ID=$(\
+    aws --profile ${PROFILE} ec2 describe-instances \
+    --filter "Name=tag:Name,Values=${INSTANCE_NAME}" \
+    --query "Reservations[].Instances[?State.Name == 'running'].InstanceId[]" \
+    --output text \
+)
+
+if [[ -z ${INSTANCE_ID} ]]; then
+    echo "Instância ${INSTANCE_NAME} não foi encontrada"
+    exit 1
+fi
+
+aws --profile ${PROFILE} ssm start-session --target ${INSTANCE_ID} \
+--document-name AWS-StartPortForwardingSession \
+--parameters '{"portNumber":["3001"],"localPortNumber":["3002"]}'
