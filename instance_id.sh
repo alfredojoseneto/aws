c09b2de9f194b217a7dc744e3c3f127dc76b727f
alfredojoseneto <alfredojoseneto@gmail.com>
2025-03-09 19:58:01 +0000

    REFACT: created shell script to retrieve id of running instances based on name

---
+++ instance_id.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+retrieve_id(){
+    profile="${1}"
+    instance_name="${2}"
+    id=$(\
+        aws --profile "${profile}" ec2 describe-instances \
+        --filter "Name=tag:Name,Values=${instance_name}" \
+        --query "Reservations[].Instances[?State.Name == 'running'].InstanceId[]" \
+        --output "text" \
+    )
+    echo "${id}"
+}
+
