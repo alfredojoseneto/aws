546bbc1db620c4de88d828c400ce3975085a1516
alfredojoseneto <alfredojoseneto@gmail.com>
2025-03-09 20:52:28 +0000

    ADD: added shell script do identify instance state based on isntance name

---
+++ instance_state.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# import scripts
+. instance_id.sh
+
+retrieve_state() {
+    profile="${1}"
+    instance_name="${2}"
+    instance_id=$(retrieve_id "${profile}" "${instance_name}")
+    state=$(\
+        aws --profile formacao-aws ec2 describe-instances \
+        --filter "Name=tag:Name,Values=bia-dev" \
+        --query "Reservations[].Instances[?InstanceId=='"${instance_id}"'].State.Name" \
+        --output text \
+    )
+
+    echo "${state}"
+
+}
