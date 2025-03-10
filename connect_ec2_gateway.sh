5d4d2305ac7e9b476d56c8a8deaa2923ae09f6aa
alfredojoseneto <alfredojoseneto@gmail.com>
2025-03-10 00:48:53 +0000

    REFACT: adjusted connect_ec2_gateway

--- connect_ec2_gateway.sh
+++ connect_ec2_gateway.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # importing scripts
-. instance_id.sh 
-. instance_state.sh
-. start_instance.sh
-. stop_instance.sh
-. tunnel_ec2.sh
+. scripts/instance_id.sh 
+. scripts/instance_state.sh
+. scripts/start_instance.sh
+. scripts/stop_instance.sh
+. scripts/tunnel_ec2.sh
 
 
 # setting variables
@@ -16,10 +16,7 @@ ec2_port_number=3001
 
 
 # validating the inputed information
-if [[ -z "${profile}" || -z "${instance_name}" ]]; then
-    echo "Primeiro parâmetro 'PROFILE' ou segundo parâmetro 'INSTANCE_NAME' não foram informados"
-    exit 1
-fi
+[[ -z "${profile}" || -z "${instance_name}" ]] && echo "Primeiro parâmetro 'PROFILE' ou segundo parâmetro 'INSTANCE_NAME' não foram informados" && exit 1
 
 
 # retrive instance id
@@ -35,15 +32,17 @@ state=$(retrieve_state "${profile}" "${instance_name}")
 
 
 # validate the instance state
-[[ -z "${state}" ]] && echo "Instância ${instance_name} não encontrada"
+[[ -z "${state}" ]] && echo "Instância ${instance_name} não encontrada" && exit 1
 
 
-# verificando se a instância já está em execução
-if [[ "${state}" == "running" ]]; then
-    echo "Instância EC2 ${instance_name} já está em execução"
-    echo "Estabelecendo tunnel com EC2 ${instance_name}"
-    tunnel_ec2 $profile $instance_name $local_port_number $ec2_port_number
+# verify the instance state to connect to it through ec2 tunnel
+if [[ "${state}" != "running" ]]; then
+    start "${profile}" "${instance_name}"
 fi
+
+echo "Instância EC2 ${instance_name} já está em execução"
+echo "Estabelecendo tunnel com EC2 ${instance_name}"
+tunnel_ec2 $profile $instance_name $local_port_number $ec2_port_number
 
 
 exit 0
